b8f9cf6040a01c3427f401fb9fd042d4cbc3542f
modern_thanadon <monologyza_za@hotmail.com>
2025-02-17 15:34:24 +0700

    [implement]: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('A-Board API')
+    .setDescription('The A-Board API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
 }
+
 bootstrap();
